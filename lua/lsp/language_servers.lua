21b0338bc80ed73b06a29e12d30433e7fefda3f0
Carlos Gomez <carlosandres0741@gmail.com>
2023-04-18 15:20:37 -0500

    fix:lua error

--- lua/lsp/language_servers.lua
+++ lua/lsp/language_servers.lua
@@ -1,20 +1,5 @@
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local lsp_installer = require("nvim-lsp-installer")
-
-local system_name
-if vim.fn.has("mac") == 1 then
-  system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-  system_name = "Linux"
-elseif vim.fn.has('win32') == 1 then
-  system_name = "Windows"
-else
-  print("Unsupported system for sumneko")
-end
---
--- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
-local sumneko_root_path = '/home/carandev/.config/nvim/lua-language-server'
-local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
@@ -69,7 +54,7 @@ local lang_servers = {
   'html',
   'tsserver',
   'emmet_ls',
-  'sumneko_lua',
+  'lua_ls',
   'jsonls',
   'tailwindcss',
   'dockerls',
@@ -87,16 +72,13 @@ local lang_servers = {
 }
 
 for _, server in ipairs(lang_servers) do
-  if server == 'sumneko_lua' then
-    require'lspconfig'[server].setup {
-      cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  if server == 'lua_ls' then
+    require'lspconfig'.lua_ls.setup {
       settings = {
         Lua = {
           runtime = {
             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
             version = 'LuaJIT',
-            -- Setup your lua path
-            path = runtime_path,
           },
           diagnostics = {
             -- Get the language server to recognize the `vim` global
@@ -105,14 +87,13 @@ for _, server in ipairs(lang_servers) do
           workspace = {
             -- Make the server aware of Neovim runtime files
             library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty =  false
           },
           -- Do not send telemetry data containing a randomized but unique identifier
           telemetry = {
             enable = false,
           },
         },
-      }
+      },
     }
   elseif server == 'phpactor' then
     require'lspconfig'[server].setup {
